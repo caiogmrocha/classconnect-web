f794f6d56770fc617384d72145609e99fffbb490
caiogmrocha <caioguilherme739@gmail.com>
2024-02-25 00:06:38 -0300

    chore(CC-103): adicionar o shadcn-vue

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,15 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+import tailwind from "tailwindcss"
+import autoprefixer from "autoprefixer"
+
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   plugins: [
     vue(),
   ],
