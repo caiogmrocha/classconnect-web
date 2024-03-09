41da16f78f024666bd1922d0651c304bbd852c20
caiogmrocha <caioguilherme739@gmail.com>
2024-03-09 17:36:19 -0300

    feat(CC-106): definir padrão para cada item da listagem de salas

--- src/views/salas/ListagemView/index.vue
+++ src/views/salas/ListagemView/index.vue
@@ -1,8 +1,26 @@
 <script setup lang="ts">
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
 </script>
 
 <template>
-    <main class="h-full flex-1 p-5">
-        <h1>Hello world</h1>
-    </main>
+    <div class="flex flex-1 h-full w-full p-4">
+        <main class="grid grid-cols-4 gap-4 w-full h-full">
+            <Card class="max-h-60 border-2 hover:border-2 hover:cursor-pointer transition-all hover:border-blue-500">
+                <CardHeader class="flex flex-row items-center space-y-0 pb-2">
+                    <CardTitle class="text-xl font-bold">
+                        Sala 1
+                    </CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <div class="text-lg font-medium">
+                        Nome do Professor
+                    </div>
+                    <p class="text-xs text-muted-foreground">
+                        Descrição da sala
+                    </p>
+                </CardContent>
+            </Card>
+        </main>
+    </div>
 </template>
