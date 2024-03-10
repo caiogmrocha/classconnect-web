58335bb82450b72260fd68972aa8a6ed157e2673
caiogmrocha <caioguilherme739@gmail.com>
2024-03-09 22:58:24 -0300

    feat(CC-74): possibilitar a troca de tema entre escuro e claro

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,16 @@
 import './assets/index.css';
 
 import { RouterView } from 'vue-router';
+
 import Toaster from './components/ui/toast/Toaster.vue';
+import { ToggleTheme } from './components/custom/toggle-theme';
 </script>
 
 <template>
+  <div class="relative">
+    <ToggleTheme class="absolute top-4 right-4 z-50" />
+  </div>
+
   <RouterView class="d-flex items-center justify-center h-[100vh] w-[100vw]"/>
 
   <Toaster />
